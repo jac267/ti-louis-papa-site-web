3153cc23f9f804a19041815b85487a52b2bebe42
jac267 <jackpotchap@gmail.com>
2022-03-10 18:53:17 -0500

    devrait marcher

--- script/language-selector.js
+++ script/language-selector.js
@@ -1,17 +1,30 @@
-var language;
-function getLanguage() {
-  localStorage.getItem("language") == null ? setLanguage("en") : false;
-  $.ajax({
-    url: "/language/" + localStorage.getItem("language") + ".json",
-    dataType: "json",
-    async: false,
-    dataType: "json",
-    success: function (lang) {
-      language = lang;
-    },
+function setLanguage() {
+  var lang = localStorage.getItem("language");
+
+  $.getJSON("language/fren.json", function (json) {
+    for (const [key, value] of Object.entries(json)) {
+      if (lang == "fr") {
+        document.body.innerHTML = document.body.innerHTML.replace(value, key);
+      }
+      if (lang == "en") {
+        document.body.innerHTML = document.body.innerHTML.replace(key, value);
+      }
+    }
   });
 }
-function setLanguage(lang) {
+function changeLanguage() {
+  current_L = localStorage.getItem("language");
+  console.log(current_L);
+
+  if (current_L == "en") {
+    localStorage.setItem("language", "fr");
+    document.getElementById("LanguageButton").textContent = "EN";
+  }
+  if (current_L == "fr") {
+    localStorage.setItem("language", "en");
+    document.getElementById("LanguageButton").textContent = "FR";
+  }
+
   console.log(document.body.innerHTML);
-  localStorage.setItem("language", lang);
+  setLanguage();
 }

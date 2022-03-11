c3ea474a76ac200bd61c1afe001b2600b55988ff
jac267 <jackpotchap@gmail.com>
2022-03-10 19:07:06 -0500

    sa devrais marcher

--- script/language-selector.js
+++ script/language-selector.js
@@ -3,10 +3,10 @@ function setLanguage() {
 
   $.getJSON("language/fren.json", function (json) {
     for (const [key, value] of Object.entries(json)) {
-      if (lang == "fr") {
+      if (lang == "fr-FR") {
         document.body.innerHTML = document.body.innerHTML.replace(value, key);
       }
-      if (lang == "en") {
+      if (lang == "en-EN") {
         document.body.innerHTML = document.body.innerHTML.replace(key, value);
       }
     }
@@ -16,12 +16,12 @@ function changeLanguage() {
   current_L = localStorage.getItem("language");
   console.log(current_L);
 
-  if (current_L == "en") {
-    localStorage.setItem("language", "fr");
+  if (current_L == "en-EN") {
+    localStorage.setItem("language", "fr-FR");
     document.getElementById("LanguageButton").textContent = "EN";
   }
-  if (current_L == "fr") {
-    localStorage.setItem("language", "en");
+  if (current_L == "fr-FR") {
+    localStorage.setItem("language", "en-EN");
     document.getElementById("LanguageButton").textContent = "FR";
   }
 

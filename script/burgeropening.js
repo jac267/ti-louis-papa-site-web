ad8f2ad11c1ffb8d4130dd8350711949e859621d
jac267 <jackpotchap@gmail.com>
2022-03-16 15:50:51 -0400

    menu deroulant

--- script/burgeropening.js
+++ script/burgeropening.js
@@ -1,7 +1,12 @@
-function openBurger() {
-  alert("opening burger");
-}
-
-function closeBurger() {
-  alert("closing burger");
+function openCloseBurger() {
+  var burger = document.getElementById("burger-menu");
+  if (burger.style.display == "block") {
+    burger.style.display = "none";
+    document.getElementById("fa-xmark").style.display = "none";
+    document.getElementById("fa-bars").style.display = "block";
+  } else {
+    burger.style.display = "block";
+    document.getElementById("fa-bars").style.display = "none";
+    document.getElementById("fa-xmark").style.display = "block";
+  }
 }

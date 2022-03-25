daea697331b368ced4ef07b851d22ac09df22072
jac267 <jackpotchap@gmail.com>
2022-03-25 10:48:46 -0400

    fix du menu en phone land scape

--- script/burgeropening.js
+++ script/burgeropening.js
@@ -5,8 +5,8 @@ function openCloseBurger() {
     document.getElementById("fa-xmark").style.display = "none";
     document.getElementById("fa-bars").style.display = "block";
   } else {
-    burger.style.display = "block";
     document.getElementById("fa-bars").style.display = "none";
     document.getElementById("fa-xmark").style.display = "block";
+    burger.style.display = "block";
   }
 }
